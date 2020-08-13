63150b17416f3c79207140b403fe7a00af83a1dc
Demii871023 <69564621+Demii871023@users.noreply.github.com>
2020-08-13 11:42:20 +0800

    20208013

--- script/index.js
+++ script/index.js
@@ -1,12 +1,21 @@
 // const.js
-//const cw = 800;
-//const ch = 450;
+
 const cw = $(window).width();
 const ch = $(window).height();
 const playerScale = 0.3;
 const scale = 0.6;
 const monsterScale = 0.7;
-var Subject = ['語文', '自然科學', '綜合活動', '數學', '科技', '健康與體育', '社會', '藝術'];
+
+var subjectN = 8;
+
+// 課目類別名稱及生成座標
+var subject_name = ['語文', '自然科學', '綜合活動', '數學', '科技', '健康與體育', '社會', '藝術'];
+var subject_xy = [[x: 0, y: 0], [x: 0, y: 0], [x: 0, y: 0], [x: 0, y: 0], [x: 0, y: 0], [x: 0, y: 0], [x: 0, y: 0], [x: 0, y: 0], [x: 0, y: 0], [x: 0, y: 0]]
+
+// 取得亂數函式
+const getRandom = (range, start) =>{
+    return Math.floor(Math.random() * (range - start + 1)) + start;
+};
 
 
 
@@ -33,7 +42,7 @@ window.addEventListener('keypress', function(e) {
       // $("#tt").click();
       // //$('#myModal').modal('show');
       // console.log("Hello"); //$('#exampleModal').modal('show');
-      // $("#subject_name").text(Subject[1]);
+      // $("#subject_name").text(subject_name[1]);
   }
     
   // 按 Q 即丟棄已選擇科目
@@ -43,7 +52,7 @@ window.addEventListener('keypress', function(e) {
       $("#tt").click();
       //$('#myModal').modal('show');
       console.log("Hello"); //$('#exampleModal').modal('show');
-      $("#subject_name").text(Subject[1]);
+      $("#subject_name").text(subject_name[1]);
   }
 }, false);
 
@@ -79,17 +88,28 @@ const subjectSelect = {
     },
     create: function(){
 
+        
         this.bg2 = this.add.sprite(cw/2,ch/2, 'bg2');
 
         this.player = this.physics.add.sprite(150, 150, 'player');
         // 角色落地的時候會彈跳
         this.player.setCollideWorldBounds(true); //角色邊界限制
         //this.player.setBounce(1); //設定彈跳值
-      
-      
         this.player.setScale(playerScale);
+      
+      
+        for(var i = 0 ; i < subjectN ; i++)
+        {
+            let tempX = getRandom($(window).width, 0-($(window).width / 2 ));
+            let tempY = getRandom($(window).height, 0-($(window).height / 2));
+          
+            console.log(tempX, tempY);
+        }
+      
+      
+      
         
-        //this.add.text(cw/2,ch/2, Subject[2], {color: "#123455", fontSize:'60px'});
+        //this.add.text(cw/2,ch/2, subject_name[2], {color: "#123455", fontSize:'60px'});
     },
     update: function(){
         let keyboard = this.input.keyboard.createCursorKeys();
@@ -109,12 +129,12 @@ const subjectSelect = {
             this.player.setVelocityY(-160);
             console.log("上面");
         }
-        if(keyboard.esc.isDown)
-        {
-            this.player.setVelocityX(0);
-            this.player.setVelocityY(0);
-            console.log("下面");
-        }
+//         if(keyboard.esc.isDown)
+//         {
+//             this.player.setVelocityX(0);
+//             this.player.setVelocityY(0);
+//             console.log("下面");
+//         }
     }
 }
 
