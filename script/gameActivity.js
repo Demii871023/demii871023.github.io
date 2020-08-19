4903752beffb4e0f04dc9e7966597a0a37c455cf
Demii871023 <69564621+Demii871023@users.noreply.github.com>
2020-08-19 13:31:36 +0800

    20200819

---
+++ script/gameActivity.js
@@ -0,0 +1,18 @@
+/*
+  scene：gameActivity.js
+  describe：當玩家點選進入課外活動，則進入此頁面
+*/
+
+
+const gameActivity = {
+    key: 'gameActivity',
+    preload: function(){
+
+    },
+    create: function(){
+
+    },
+    update: function(){
+        
+    }
+}
