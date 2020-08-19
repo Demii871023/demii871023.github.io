82d902a05e93576cec0225b2c33bf50658939232
Demii871023 <69564621+Demii871023@users.noreply.github.com>
2020-08-19 13:29:28 +0800

    20200819

---
+++ script/gameBouns.js
@@ -0,0 +1,23 @@
+/*
+scene：gameBouns.js
+decribe：當玩家進入 bouns 門時，進入獎勵頁面
+
+
+
+*/
+
+
+
+
+const gameBouns = {
+    key: 'gameBouns',
+    preload: function(){
+
+    },
+    create: function(){
+
+    },
+    update: function(){
+        
+    }
+}
