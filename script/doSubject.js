858ac3fc18e27182fac20097471ffe1d70615d38
Demii871023 <69564621+Demii871023@users.noreply.github.com>
2020-08-25 11:51:57 +0800

    20200825

--- script/doSubject.js
+++ script/doSubject.js
@@ -69,10 +69,10 @@ const doSubject = {
         
     },
     create: function(){
-        doAText = this.add.text(cw/2, ch/2, subject_choice[subjectTmp].A, {color: "#FFFFFF", fontSize:'45px'});
-        doBText = this.add.text(cw/2, ch/2, subject_choice[subjectTmp].B, {color: "#FFFFFF", fontSize:'45px'});
-        doCText = this.add.text(cw/2, ch/2, subject_choice[subjectTmp].C, {color: "#FFFFFF", fontSize:'45px'});
-        doDText = this.add.text(cw/2, ch/2, subject_choice[subjectTmp].D, {color: "#FFFFFF", fontSize:'45px'});
+        doAText = this.add.text(cw/10, ch/2-60, subject_choice[subjectTmp].A, {color: "#FFFFFF", fontSize:'25px'});
+        doBText = this.add.text(cw/10, ch/2-20, subject_choice[subjectTmp].B, {color: "#FFFFFF", fontSize:'45px'});
+        doCText = this.add.text(cw/10, ch/2+20, subject_choice[subjectTmp].C, {color: "#FFFFFF", fontSize:'45px'});
+        doDText = this.add.text(cw/10, ch/2+60, subject_choice[subjectTmp].D, {color: "#FFFFFF", fontSize:'45px'});
             
     },
     update: function(){
