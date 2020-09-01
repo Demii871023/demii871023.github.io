3c4335288273a272f106c119f5c02e64902c6863
Demi871023 <43329497+Demi871023@users.noreply.github.com>
2020-09-01 15:58:15 +0800

    20200901

--- script/recordUpload.js
+++ script/recordUpload.js
@@ -5,7 +5,18 @@ var player_record = [
 const recordUpload = {
     key: 'recordUpload',
     preload: function(){
-        this.load.image('bg1', 'image/Background/jungle-clipart-background-6.jpg'); 
+        this.load.image('bg1', 'image/Background/jungle-clipart-background-6.jpg');
+	    
+	    
+	// 8 個課業學科
+        this.load.image('Langugage', 'image/18College/13.png');
+        this.load.image('Science', 'image/18College/11.png');
+        this.load.image('Integrative', 'image/18College/7.png');
+        this.load.image('Math', 'image/18College/3.png');
+        this.load.image('Technology', 'image/18College/8.png');
+        this.load.image('Health', 'image/18College/4.png');
+        this.load.image('Social', 'image/18College/12.png');
+        this.load.image('Art', 'image/18College/9.png');
         
     },
     create: function(){
@@ -13,11 +24,25 @@ const recordUpload = {
         bg1 = this.add.sprite(cw/2, ch/2, 'bg1');
         bg1.setScale(4);
 	    
+	
+	recordGroup = this.physics.add.group();
+	recordGroupChild = recordGroup.getChildren();
+	    
+	    
+	// 生成 課業學科遮罩
 	maskSubject = this.add.graphics()
         maskSubject.fillStyle(0x000000, 0.5).fillRect(5, 0, cw/2-10, ch);
+	// 生成 課業學科紀錄豆
+	for(var i = 0 ; i < player_record.length ; i++)
+	{
+	    recordGroup.create(cw/4, ch/4, player_record[i].subject);
+	}
 	
+	
+	// 生成 課外活動遮罩
 	maskActivity = this.add.graphics()
         maskActivity.fillStyle(0x000000, 0.5).fillRect(cw/2+5, 0, cw/2-10, ch);
+	// 生成 課外活動紀錄豆 
 
 
     },
