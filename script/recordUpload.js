2ad9b43536dad56da6c681123e42d1fefa970020
Demi871023 <43329497+Demi871023@users.noreply.github.com>
2020-09-01 15:37:33 +0800

    20200901

--- script/recordUpload.js
+++ script/recordUpload.js
@@ -2,6 +2,10 @@ var player_record = [
 ]
 
 
+const cw = $(window).width();
+const ch = $(window).height();
+
+
 
 const recordUpload = {
     key: 'recordUpload',
@@ -10,16 +14,22 @@ const recordUpload = {
         
     },
     create: function(){
+	    
+	mask = this.add.graphics()
+        mask.fillStyle(0x000000, 0.5).fillRect(0, 0, cw, ch);
         bg1 = this.add.sprite(cw/2, ch/2, 'bg1');
         bg1.setScale(4);
 	    
 	
 	
-	maskSubject = this.add.graphics()
-        maskSubject.fillStyle(0x000000, 0.5).fillRect(0, 0, 100, 500);
+// 	maskSubject = this.add.graphics()
+//         maskSubject.fillStyle(0x000000, 0.5).fillRect(0, 0, 100, 500);
 	
-	maskActivity = this.add.graphics()
-        maskActivity.fillStyle(0x000000, 0.5).fillRect(300, 0, 100, 500);
+// 	maskActivity = this.add.graphics()
+//         maskActivity.fillStyle(0x000000, 0.5).fillRect(300, 0, 100, 500);
+	
+	mask = this.add.graphics()
+        mask.fillStyle(0x000000, 0.5).fillRect(0, 0, cw, ch);
 	
         
 	    
