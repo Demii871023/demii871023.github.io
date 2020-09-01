499fe88aeb4e2e9a5c73446a0ffbc66023c2d3c6
Demi871023 <43329497+Demi871023@users.noreply.github.com>
2020-09-01 15:28:23 +0800

    20200901

--- script/recordUpload.js
+++ script/recordUpload.js
@@ -6,12 +6,19 @@ var player_record = [
 const recordUpload = {
     key: 'recordUpload',
     preload: function(){
-        this.load.image('bg1', 'image/Background/jungle-clipart-background-6.jpg');
+        this.load.image('bg1', 'image/Background/jungle-clipart-background-6.jpg'); 
         
     },
     create: function(){
         bg1 = this.add.sprite(cw/2, ch/2, 'bg1');
         bg1.setScale(4);
+	    
+	maskSubject = this.add.graphics(0, 0, 100, 500);
+        maskSubject.fillStyle(0x000000);
+	    
+	    
+	maskActiviy = this.add.graphics(200, 0, 100, 500);
+        maskSubject.fillStyle(0x000000);
         
 	    
     },
