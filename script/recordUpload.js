852056bb76592898fc9ff0304d3912e6f6afe227
Demi871023 <43329497+Demi871023@users.noreply.github.com>
2020-09-02 15:01:29 +0800

    20200902

--- script/recordUpload.js
+++ script/recordUpload.js
@@ -105,7 +105,7 @@ const recordUpload = {
 	    }
 	
 	    // 加入記錄說明文字物件，並將其存放進入陣列裡面且文字 alpha 參數設為零，成為透明文字。linespacing 為行距
-	    tempText = this.add.text(recordGroupChild[i].x + 50, recordGroupChild[i].y + 5, player_record[i].option, {color: "#f7f7f7", fontSize:'20px', lineSpacing: 10, wordWrap: { width: 470, useAdvancedWrap: true }});
+	    tempText = this.add.text(recordGroupChild[i].x + 50, recordGroupChild[i].y + 5, player_record[i].option, {color: "#f7f7f7", fontSize:'20px', lineSpacing: 10, wordWrap: { width: 400, useAdvancedWrap: true }});
 	    
 	    recordGroup_optionOB[i] = tempText;
 	    recordGroup_optionOB[i].alpha = 0;
