6695f012bd99c4245d62b41c2301639e35d6e614
Demi871023 <43329497+Demi871023@users.noreply.github.com>
2020-09-02 13:18:01 +0800

    20200902

--- script/recordUpload.js
+++ script/recordUpload.js
@@ -72,7 +72,7 @@ const recordUpload = {
 	    // 生成豆子的影子
 	    shadowGroup.create(cw/4 - 100, ch/4 + 100*i, player_record[i].subject);
             shadowGroupChild[i].setScale(0.4);
-	    shadowGroupChild[i].anchor.set(0.5);
+	    shadowGroupChild[i].setAnchor(0.5);
     	    shadowGroupChild[i].tint = 0x000000;
     	    shadowGroupChild[i].alpha = 0.6;
 	}
