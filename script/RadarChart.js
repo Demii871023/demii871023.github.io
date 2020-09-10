a108d6f3b970e2e25789dbd0a0668d8c2928818b
Demi871023 <43329497+Demi871023@users.noreply.github.com>
2020-09-10 13:39:47 +0800

    20200910

--- script/RadarChart.js
+++ script/RadarChart.js
@@ -78,9 +78,6 @@ var chartconfig = {
     }
 };
 
-// window.onload = function() {
-//     window.myRadar = new Chart(document.getElementById('RaderChartCanvas'), chartconfig);
-// };
 
 function playerConfirm(){
     myRadar = new Chart(document.getElementById('RaderChartCanvas'), chartconfig);
@@ -161,72 +158,3 @@ function playerValueControl(e) {
         }
     }
 }
-
-
-// var ctx = document.getElementById('RaderChartCanvas');
-// var RaderChart;
-
-
-
-// function playerConfirm()
-// {
-//     console.log("繪製雷達圖");
-//     RaderChart = new Chart(ctx, {
-//         type: 'radar',
-//         data: {
-//             labels:["壓力", "體力", "慣性", "人際支持"],
-//             datasets: [{
-//                 label: '角色能力值',
-//                 data:[100, 19, 34, 40],
-//                 backgroundColor: 'rgba(255, 0, 0, .3)',
-//                 borderColor: [
-//                     'rgba(255, 99, 132, 1',
-//                 ],
-//                 borderWidth: 2
-//             }]
-//         },
-//         options: {
-//             scale: {
-                
-//                 // 數值區塊
-//                 ticks: {
-//                     display: true,
-//                     beginAtZero: true,
-//                     fontSize: 12,
-//                     maxTicksLimit: 100,
-//                     stepSize:20,
-//                     backdropColor: 'rgba(255,255,255,0.5)',
-//                 },
-//                 // 發散線
-//                 angleLines:{
-//                     color: 'rgba(255,255,255,0.5)',
-//                 },
-//                 // 四角線
-//                 gridLines: {
-//                     color: 'rgba(255,255,255,0.5)',
-//                 },
-//                 // 頂點標籤
-//                 pointLabels:{
-//                     // fontColor: 'rgba(255,255,255,0.5)',
-//                     fontColor: ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
-//                     fontSize: 20,
-//                 },
-//             }
-//         }
-//     });
-
-//     // 關掉 datasets 的 label
-//     RaderChart.options.legend.display = false;
-//     // console.log(RaderChart.data.labels[0]);
-//     // RaderChart.options.scale.pointLabels[0] = '#000000';
-//     // updateData();
-// }
-
-// function updateData()
-// {
-//     console.log("繪製雷達圖");
-    
-
-// }
-
-
